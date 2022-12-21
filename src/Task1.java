f0c7c62dedf973147bd0787e8d51ec77f2f5ffeb
Natalia <67316260+npolyakova@users.noreply.github.com>
2022-12-21 16:59:00 +0300

    Update Task1.java

--- src/Task1.java
+++ src/Task1.java
@@ -1,12 +1,12 @@
 public class Task1 {
-    //Найти длину окружности радиуса 4
-
+    //Найти длину окружности радиуса 4. L = 2Pir
     public static void main(String[] args) {
-        int r = 4;
-
-        double l = Math.PI * Math.pow(r, 2) + 0.4;
+        //short r = 4;
+        System.out.println("Введите радиус");
+        Scanner sc = new Scanner(System.in);
+        short r = sc.nextShort();
+        double l = 2 * Math.PI * r;
 
         System.out.println("Длина окружности = " + l);
-        System.out.println("Длина окружности округленная = " + Math.round(l));
     }
 }
