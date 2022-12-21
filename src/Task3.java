c6ee8ea860197b6fa774b86a83d15757e91e5e9f
Natalia <67316260+npolyakova@users.noreply.github.com>
2022-12-21 16:59:56 +0300

    Update Task3.java

--- src/Task3.java
+++ src/Task3.java
@@ -7,24 +7,20 @@ public class Task3 {
     //    2. число десятков в нем;
     //    3. сумму его цифр;
     //    4. произведение его цифр.
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите трехзначное число");
         int number = sc.nextInt();
-        sc.close();
 
+        //1 2 3
+        //123 % 10 = 3
         int units = number % 10;
+        //double a = 123 / 10 = 12.3
+        //int a = 123 / 10 = 12
         int decimals = (number / 10) % 10;
-        int hundreds = number / 100;
+        int hundreds = (number / 10) / 10;
 
-        int sum = units + decimals + hundreds;
-        int product = units * decimals * hundreds;
-
-        System.out.println("Единицы = " + units);
-        System.out.println("Десятки = " + decimals);
-        System.out.println("Сотни = " + hundreds);
-        System.out.println("Сумма = " + sum);
-        System.out.println("Произведение = " + product);
+        System.out.println("Cумма цифр = " + (units + decimals + hundreds));
+        System.out.println("Произведение цифр = " + (units * decimals * hundreds));
     }
 }
